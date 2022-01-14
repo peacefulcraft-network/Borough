3c8e42b389390a395925312e2b44552b9240cae0
BrokenKage <47730240+BrokenKage@users.noreply.github.com>
2022-01-14 18:42:14 -0500

    Update PlayerCacheBlockListeners.java

--- src/net/peacefulcraft/borough/listeners/PlayerCacheBlockListeners.java
+++ src/net/peacefulcraft/borough/listeners/PlayerCacheBlockListeners.java
@@ -1,25 +1,35 @@
 package net.peacefulcraft.borough.listeners;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.data.Directional;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockBurnEvent;
 import org.bukkit.event.block.BlockCanBuildEvent;
+import org.bukkit.event.block.BlockDispenseEvent;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.block.BlockFertilizeEvent;
+import org.bukkit.event.block.BlockFromToEvent;
 import org.bukkit.event.block.BlockIgniteEvent;
 import org.bukkit.event.block.BlockPistonExtendEvent;
 import org.bukkit.event.block.BlockPistonRetractEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.EntityBlockFormEvent;
 
 import net.peacefulcraft.borough.Borough;
 import net.peacefulcraft.borough.event.executor.BoroughActionExecutor;
 import net.peacefulcraft.borough.storage.BoroughChunk;
+import net.peacefulcraft.borough.utilities.ItemLists;
 
 public class PlayerCacheBlockListeners implements Listener {
 	
@@ -91,6 +101,66 @@ public class PlayerCacheBlockListeners implements Listener {
 				}
 			});
 		}
+	}
+
+	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+	public void onCreateExplosion(BlockExplodeEvent ev) {
+		Material mat = ev.getBlock().getType();
+
+		List<Block> blocks = BoroughActionExecutor.filterExplodeableBlocks(ev.blockList(), mat, null, ev);
+		ev.blockList().clear();
+		ev.blockList().addAll(blocks);
+		if (ev.blockList().isEmpty()) { return; }
+
+		// TODO: Further processing for location specifics and config
+	}
+
+	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+	public void onFrostWalker(EntityBlockFormEvent ev) {
+		if (ev.getEntity() instanceof Player) {
+			ev.setCancelled(!BoroughActionExecutor.canBuild((Player)ev.getEntity(), ev.getBlock().getLocation(), ev.getBlock().getType()));
+		}
+	}
+
+	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+	public void onBlockFromTo(BlockFromToEvent ev) {
+		if (ev.getBlock().getType() == Material.DRAGON_EGG) { return; }
+
+		if (!canBlockMove(ev.getBlock(), ev.getToBlock(), true)) {
+			ev.setCancelled(true);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+	public void onBlockDispense(BlockDispenseEvent ev) {
+		if (ev.getBlock().getType() != Material.DISPENSER) { return; }
+
+		Material mat = ev.getItem().getType();
+
+		if (ItemLists.BUCKETS.contains(mat.name())) { return; }
+
+		if (!ItemLists.BUCKETS.contains(mat.name()) && mat != Material.BONE_MEAL && mat != Material.HONEYCOMB) { return; }
+
+		if (!canBlockMove(ev.getBlock(), ev.getBlock().getRelative(((Directional) ev.getBlock().getBlockData()).getFacing()), true)) {
+			ev.setCancelled(true);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+	public void onFertilize(BlockFertilizeEvent ev) {
+		List<BlockState> allowed = new ArrayList<>();
+		Player p = ev.getPlayer();
+
+		ev.getBlocks().forEach((state) -> {
+			Block block = state.getBlock();
+			BoroughChunk chunk = Borough.getClaimStore().getChunk(block.getLocation());
+			if (chunk.isChunkClaimed() && chunk.getClaimMeta().getOwners().contains(p.getUniqueId())) {
+				allowed.add(state);
+			}
+		});
+
+		ev.getBlocks().clear();
+		ev.getBlocks().addAll(allowed);
 	}
 
 	/**
